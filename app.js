20a4693eaeebcb3a619736ee9977d4ba3db9ae55
eranagk <142158491+eranagk@users.noreply.github.com>
2025-02-14 09:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '2';
+const version = '1';
 
 app.get('/', (req, res) => {
     // set response content    
@@ -11,6 +11,8 @@ app.get('/', (req, res) => {
                     <body>
                         <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]:Codepipeline Test KT session 2025 Lankacom</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                        <br>    
+                        <br>    
                         <br>    
                         <img src="https://picsum.photos/400/400?random=1">
                         </div>
